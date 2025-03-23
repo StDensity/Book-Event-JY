cfe30548e33e0333c99692f83df6e758130657e3
StDensity <noyaljosephnnj@gmail.com>
2025-03-23 03:29:19 +0100

    test(heroimage): Testing hero image not visible on deployment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Hero = () => {
   return (
-    <div className="h-[30vh] bg-[url('/images/Hero.png')] bg-cover md:h-[54vh] md:bg-[center_-200px]">
+    <div className="h-[30vh] bg-[url('https://raw.githubusercontent.com/StDensity/Book-Event-JY/refs/heads/master/public/images/hero.png')] bg-cover md:h-[54vh] md:bg-[center_-200px]">
       <Navbar />
     </div>
   );
