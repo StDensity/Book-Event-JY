4c8117fcf1a3e6652840a26cff0a14069c7a2fd5
StDensity <noyaljosephnnj@gmail.com>
2025-03-23 03:49:52 +0100

    feat(open graph): Added OG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,32 @@
 import ContentArea from "@/components/ContentArea";
 import Hero from "@/components/Hero";
 import RegisterFooter from "@/components/RegisterFooter";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "NFT Workshop - Book Event",
+  description: "Learn how to buy NFT in India. All about NFT marketplaces",
+  openGraph: {
+    title: "NFT Workshop - Book Event",
+    description: "Learn how to buy NFT in India. All about NFT marketplaces",
+    images: [
+      {
+        url: "/images/Hero.png",
+        width: 1200,
+        height: 630,
+        alt: "NFT Workshop",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "NFT Workshop - Book Event",
+    description: "Learn how to buy NFT in India. All about NFT marketplaces",
+    images: ["/images/Hero.png"],
+  },
+};
 
 export default function Home() {
   return (
