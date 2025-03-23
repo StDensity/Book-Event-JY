0af9acfbe8543940cdad7e3680fe63aa5f5bf3ed
StDensity <noyaljosephnnj@gmail.com>
2025-03-23 03:13:06 +0100

    test(heroimage): Testing hero image not visible on deployment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Hero = () => {
   return (
-    <div className="h-[30vh] bg-[url('/images/Hero.png')] bg-cover md:h-[54vh] md:bg-[center_-200px]">
+    <div className="h-[30vh] bg-[url('https://as2.ftcdn.net/jpg/03/14/72/47/1000_F_314724744_0N5fCAXheQZHJzFCAHgzqxPU2FAcryVh.jpg')] bg-cover md:h-[54vh] md:bg-[center_-200px]">
       <Navbar />
     </div>
   );
