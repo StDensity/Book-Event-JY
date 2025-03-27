8b099b189785e634941b73c83430579f9c8ebed5
StDestiny <84127029+StDensity@users.noreply.github.com>
2025-03-27 12:19:09 +0100

    feat(button): Added functionality for reg button

--- src/components/RegisterFooter.tsx
+++ src/components/RegisterFooter.tsx
@@ -1,3 +1,5 @@
+
+import Link from "next/link";
 import React from "react";
 
 const RegisterFooter = () => {
@@ -24,9 +26,9 @@ const RegisterFooter = () => {
           <p className="text-sm font-light">Price</p>
           <p className="text-accent-primary text-2xl font-bold">Rs. 350</p>
         </div>
-        <button className="bg-accent-primary hover:bg-accent-primary/95 hover:cursor-pointer rounded-lg px-6 py-4 text-2xl font-bold text-white">
+        <Link href={"/register"} className="bg-accent-primary hover:bg-accent-primary/95 hover:cursor-pointer rounded-lg px-6 py-4 text-2xl font-bold text-white">
           Register Now
-        </button>
+        </Link>
       </div>
     </div>
   );
